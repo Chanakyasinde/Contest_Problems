541d4a475e247db638c906a50ebfc1883d905d23
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-07 20:04:05 +0530

    Update matrix_crosswalk.py

--- Python_Problems/matrix_crosswalk.py
+++ Python_Problems/matrix_crosswalk.py
@@ -1,1 +1,12 @@
-
+n=int(input())
+l=[]
+mid=n//2
+result=0
+for i in range(n):
+    s = list(map(int,input().split()))
+    l.append(s)
+for i in range(n):
+    result+=l[i][mid]
+    result+=l[mid][i]
+result-=l[mid][mid]
+print(result)
