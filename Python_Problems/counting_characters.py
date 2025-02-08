db88a42eb8660410fc053f03eab504295d46321d
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-08 16:21:36 +0530

    Update counting_characters.py

--- Python_Problems/counting_characters.py
+++ Python_Problems/counting_characters.py
@@ -1,1 +1,20 @@
 # by using prefix sum technique
+n,q=map(int,input().split())
+s=input()
+m=26
+matrix = [[0] * n for _ in range(m)]
+for i in range(m):
+    for j in range(n):
+        if ord(s[j])-97==i:
+            matrix[i][j]=matrix[i][j-1]+1
+        else:
+            matrix[i][j]=matrix[i][j-1]
+
+for i in range(q):
+    l,r,c=map(str,input().split())
+    l=int(l)
+    r=int(r)
+    if l==0:
+        print(matrix[ord(c)-97][r])
+    else:
+        print(matrix[ord(c)-97][r]-matrix[ord(c)-97][l-1])
