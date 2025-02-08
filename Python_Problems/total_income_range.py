a81c9f11162c6e978a3549d8c509bb9343308f60
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-08 16:22:58 +0530

    Update total_income_range.py

--- Python_Problems/total_income_range.py
+++ Python_Problems/total_income_range.py
@@ -1,1 +1,17 @@
 # prefix sum technic
+
+n=int(input())
+lst=list(map(int, input().split()))
+prefix=[0]*n
+for i in range(n):
+    if i==0:
+        prefix[i]=lst[i]
+    else:
+        prefix[i]=lst[i]+prefix[i-1]
+q=int(input())
+for _ in range(q):
+    l,r=map(int,input().split())
+    if l==0:
+        print(prefix[r])
+    else:
+        print(prefix[r]-prefix[l-1])
