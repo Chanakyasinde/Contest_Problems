b199c142506f339762c4e79b7a0031dc15fc32e6
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-08 16:17:53 +0530

    Update ending_zeros.py

--- Python_Problems/ending_zeros.py
+++ Python_Problems/ending_zeros.py
@@ -1,1 +1,18 @@
+n=int(input())
+arr=list(map(int, input().split()))
+lst=[]
+for item in arr:
+    if item!=0:
+        lst.append(item)
 
+
+
+# for it in arr:
+#     if it==0:
+#         lst.append(it)
+b = n-len(lst)
+lst.extend([0]*b)
+print(*lst)
+
+# for num in lst:
+#     print(num, end=" ")
