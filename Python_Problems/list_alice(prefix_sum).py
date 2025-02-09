9c4b76c7164bff10c7a353034e6124475a9c04a9
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-09 18:50:53 +0530

    Update list_alice(prefix_sum).py

--- Python_Problems/list_alice(prefix_sum).py
+++ Python_Problems/list_alice(prefix_sum).py
@@ -1,1 +1,19 @@
-
+n=int(input())
+lst=list(map(int, input().split()))
+prefix=[0]*n
+if lst[0]%5==0:
+    prefix[0]=1
+else:
+    prefix[0]=0
+for i in range(1,n):
+    if lst[i]%5 == 0:
+        prefix[i]=prefix[i-1]+1
+    else:
+        prefix[i]=prefix[i-1]
+q=int(input())
+for _ in range(q):
+    l,r=map(int,input().split())
+    if l == 0:
+        print(prefix[r])
+    else:
+        print(prefix[r]-prefix[l-1])
