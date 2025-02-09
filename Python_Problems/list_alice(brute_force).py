a4af74c479430685f103584aab74c7f7bacd481d
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-09 18:49:55 +0530

    Update list_alice(brute_force).py

--- Python_Problems/list_alice(brute_force).py
+++ Python_Problems/list_alice(brute_force).py
@@ -1,1 +1,10 @@
-
+n=int(input())
+lst=list(map(int, input().split())) 
+q=int(input())
+for _ in range(q):
+    l,r=map(int, input().split())
+    count=0
+    for i in range(l,r+1):
+        if lst[i]%5==0:
+            count+=1
+    print(count)
