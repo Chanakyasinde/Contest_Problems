181a2a0db9ea9404583f4124ad66d8f0f3eb1072
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-10 21:36:15 +0530

    Update power_quries.py

--- Python_Problems/power_quries.py
+++ Python_Problems/power_quries.py
@@ -7,16 +7,13 @@ def power(ans1, ans2, mod):
         ans1 = (ans1 * ans1) % mod
         ans2 //= 2
     return result
-
 n, q = map(int, input().split())
 arr = list(map(int, input().split()))
 prefix = [0] * n
 mod = (10**9) + 7
-
 prefix[0] = arr[0] % mod
 for i in range(1, n):
     prefix[i] = (prefix[i-1] + arr[i]) % mod
-
 for _ in range(q):
     l1, r1, l2, r2 = map(int, input().split())
     l1 -= 1
