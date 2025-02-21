987f98bbcb0dbdca5a65b3f9ca0a7cd7812fae57
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-21 14:42:27 +0530

    Update check_eligibility_promise.js

--- JavaScript/check_eligibility_promise.js
+++ JavaScript/check_eligibility_promise.js
@@ -1,1 +1,17 @@
+function mainFunction(age ,gender){
+     return checkMarriageEligibility(age,gender)
+     .then((data)=>console.log(data))
+    .catch((error)=>console.log(error))
+}
 
+function checkMarriageEligibility(age, gender) {
+    return new Promise((resolve,reject)=>{
+          if (age>=18 && gender=="female") {
+               resolve("You are eligible for marriage in India.")
+          } else if (age>=21 && gender=="male") {
+               resolve("You are eligible for marriage in India.")
+          } else {
+               reject("You are not eligible for marriage in India.")
+          }
+     })
+}
