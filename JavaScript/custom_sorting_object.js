ff76f365be7223dadbd6970a138f6980e22fc454
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-25 23:11:08 +0530

    Update custom_sorting_object.js

--- JavaScript/custom_sorting_object.js
+++ JavaScript/custom_sorting_object.js
@@ -1,1 +1,10 @@
-
+function sortStudents(students){
+  // write your code here
+  students=students.sort((studentA,studentB) => {
+    if (studentA.score === studentB.score) {
+        return studentA.name.localeCompare(studentB.name)
+    }
+    return studentB.score - studentA.score
+  })
+  return students
+}
