20c69419f5ed89a3a0544a9c53df685cea9b033e
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-25 23:09:20 +0530

    Update check_eligibility.js

--- JavaScript/check_eligibility.js
+++ JavaScript/check_eligibility.js
@@ -1,1 +1,14 @@
-
+function mainFunction(age){
+   checkDrivingEligibility(age)
+   .then((data) => console.log(data))
+   .catch((error) => console.log(error))
+}
+function checkDrivingEligibility(age) {
+   return new Promise((resolve,reject) => {
+      if (age>=18) {
+         resolve("You are eligible to drive in India.")
+      } else {
+         reject("You are not eligible to drive in India.")
+      }
+   })
+}
