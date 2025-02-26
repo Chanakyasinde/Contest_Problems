4382138e931cac745deb8bb96eb41a50df1fb41c
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-26 16:18:55 +0530

    Update transcation_manager.js

--- JavaScript/transcation_manager.js
+++ JavaScript/transcation_manager.js
@@ -1,1 +1,23 @@
+function processTransactions(users, transactions) {
+    let updatedUsers = users.map(user => ({ ...user })); 
 
+    for (let transaction of transactions) {
+        let sender = updatedUsers.find(user => user.id === transaction.user_id1);
+        let receiver = updatedUsers.find(user => user.id === transaction.user_id2);
+
+        if (sender) {
+            let senderBalance = sender.balance;
+            if (senderBalance >= transaction.amount) {
+                sender.balance -= transaction.amount; 
+            } else {
+                sender.balance -= transaction.amount; 
+            }
+        }
+
+        if (receiver) {
+            receiver.balance += transaction.amount; 
+        }
+    }
+
+    return updatedUsers;
+}
