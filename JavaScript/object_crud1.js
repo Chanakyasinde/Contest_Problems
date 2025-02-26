08d9c054ef57f8d499820606c37bc918bef66096
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-26 16:17:08 +0530

    Update object_crud1.js

--- JavaScript/object_crud1.js
+++ JavaScript/object_crud1.js
@@ -1,1 +1,12 @@
+function createObjectFromArray(array, id) {
+    let result = {};
 
+    for (let index = 0; index < array.length; index++) {
+        let value = array[index];
+        if (value !== null && value % 2 === 0 && index !== id) {
+            result[index] = value;
+        }
+    }
+
+    return result;
+}
