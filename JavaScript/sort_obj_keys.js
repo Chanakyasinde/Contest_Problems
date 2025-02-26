413ea1371c2824bf62ac72f41d9c2547cdfaf0b2
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-26 16:17:24 +0530

    Update sort_obj_keys.js

--- JavaScript/sort_obj_keys.js
+++ JavaScript/sort_obj_keys.js
@@ -1,1 +1,10 @@
-
+function sortObjectKeys(obj) {
+    const sortedKeys = Object.keys(obj).sort();
+    const sortedObj = {};
+    
+    for (let key of sortedKeys) {
+        sortedObj[key] = obj[key];
+    }
+    
+    return sortedObj;
+}
