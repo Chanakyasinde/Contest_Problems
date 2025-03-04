b43932d15de4124912370bbd4212318e7c5b5abb
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-04 15:25:04 +0530

    Update max_at_max.py

--- Python_Problems/max_at_max.py
+++ Python_Problems/max_at_max.py
@@ -1,1 +1,17 @@
+def count_valid_subarrays(arr, x):
+    n = len(arr)
+    count = 0
+    start = 0
+    
+    for end in range(n):
+        if arr[end] > x:
+            start = end + 1
+        else:
+            count += (end - start + 1)
+    
+    return count
 
+n, x = map(int, input().split())
+arr = list(map(int, input().split()))
+result = count_valid_subarrays(arr, x)
+print(result)
