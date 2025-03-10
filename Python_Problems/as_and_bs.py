ff49101e68099e4fa4c625bc55204073e5da4a4c
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-10 23:05:39 +0530

    Update as_and_bs.py

--- Python_Problems/as_and_bs.py
+++ Python_Problems/as_and_bs.py
@@ -1,1 +1,14 @@
-
+# Your code here
+n=int(input())
+s=input()
+a=0
+b=0
+for i in range(n):
+    if s[i]=="a":
+        a+=1
+    else:
+        b+=1
+if a>b:
+    print("Yes")
+else:
+    print("No")
